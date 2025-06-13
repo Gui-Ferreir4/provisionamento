e805d10615cb514bc19045141d1e04675f96f915
Gui-Ferreir4 <gui231112@gmail.com>
2025-06-13 09:59:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,15 @@ from datetime import datetime, date, timedelta
 from github import Github
 import holidays
 
+st.markdown("""
+    <style>
+    .main .block-container {
+        padding-left: 0rem;
+        padding-right: 0rem;
+        max-width: 100%;
+    }
+    </style>
+""", unsafe_allow_html=True)
 
 # --- CONFIGS ---
 GITHUB_USER = st.secrets["github"]["user"]
